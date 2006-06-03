b2d9ab1d29ddd26d347909d35e8e71ad90526de6
Stuart Walsh <stu@oftc.net>
2006-06-03 22:55:41 +0000

    Now we can connect to the database using the information supplied in the config file

--- services/dbm.c
+++ services/dbm.c
@@ -11,14 +11,14 @@ db_init()
   memset(&database_info, 0, sizeof(database_info));
 
   if(num_drivers >= 0)
-    printf("libdbi: loaded: %d drivers available.\n", num_drivers);
+    printf("db: loaded: %d drivers available.\n", num_drivers);
   else
   {
-    printf("libdbi: Error loading libdbi.\n");
+    printf("db: Error loading db.\n");
     exit(-1);
   }
 
-  printf("libdbi: version: %s\n", dbi_version());
+  printf("db: version: %s\n", dbi_version());
 }
 
 void
@@ -29,9 +29,28 @@ db_load_driver()
   dbconn = dbi_conn_new(database_info.driver);
   if(dbconn == NULL)
   {
-    printf("libdbi: Error loading database driver %s\n", database_info.driver);
+    printf("db: Error loading database driver %s\n", database_info.driver);
     exit(-1);
   }
 
-  printf("libdbi: Driver %s loaded\n", database_info.driver);
+  printf("db: Driver %s loaded\n", database_info.driver);
+
+
+  dbi_conn_set_option(dbconn, "username", database_info.username);
+  dbi_conn_set_option(dbconn, "password", database_info.password);
+  dbi_conn_set_option(dbconn, "dbname", database_info.dbname);
+
+  if(dbi_conn_connect(dbconn) < 0)
+  {
+    char *error;
+    dbi_conn_error(dbconn, &error);
+    printf("db: Failed to connect to database %s\n", error);
+    exit(-1);
+  }
+  else
+    printf("db: Database connection succeeded.\n");
+
+
+  dbi_conn_close(dbconn);
+  dbi_shutdown();
 }
