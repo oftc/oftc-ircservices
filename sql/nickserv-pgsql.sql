8c6bf3f5650cf4cfcf558886abeb1b1cafe04be3
Stuart Walsh <stu@oftc.net>
2006-07-02 22:00:31 +0000

    add pgsql schema(which im sure weasel will complain at :))

---
+++ sql/nickserv-pgsql.sql
@@ -0,0 +1,18 @@
+CREATE TABLE nickname (
+  id              SERIAL PRIMARY KEY,
+  nick            VARCHAR(30) NOT NULL default '',
+  password        VARCHAR(34) NOT NULL default '',
+  url             VARCHAR(255) NOT NULL default '',
+  email           VARCHAR(255) NOT NULL default '',
+  last_host       VARCHAR(255) NOT NULL default '',
+  last_realname   VARCHAR(255) NOT NULL default '',
+  last_quit       VARCHAR(255) NOT NULL default '',
+  last_quit_time  INTEGER NOT NULL default '0',
+  reg_time        INTEGER NOT NULL default '0',
+  last_seen       INTEGER NOT NULL default '0',
+  last_used       INTEGER NOT NULL default '0',
+  status          INTEGER NOT NULL default '0',
+  flags           INTEGER NOT NULL default '0',
+  language        INTEGER NOT NULL default '0',
+  UNIQUE (nick)
+);
