fcaa75fe30ff1e5f9eb693047e70ca90bb66a5bd
Stuart Walsh <stu@oftc.net>
2006-07-02 21:51:00 +0000

    Un Butchure the mysql schema

--- sql/nickserv-mysql.sql
+++ sql/nickserv-mysql.sql
@@ -1,19 +1,18 @@
-CREATE TABLE `nicknames` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `nick` varchar(30) NOT NULL default '',
-  `password` varchar(34) NOT NULL default '',
-  `url` varchar(255) NOT NULL default '',
-  `email` varchar(255) NOT NULL default '',
-  `last_host` varchar(255) NOT NULL default '',
-  `last_realname` varchar(255) NOT NULL default '',
-  `last_quit` varchar(255) NOT NULL default '',
-  `last_quit_time` int(10) NOT NULL default '0',
-  `reg_time` int(10) NOT NULL default '0',
-  `last_seen` int(10) NOT NULL default '0',
-  `last_used` int(10) NOT NULL default '0',
-  `status` int(10) unsigned NOT NULL default '0',
-  `flags` int(10) unsigned NOT NULL default '0',
-  `language` smallint(5) unsigned NOT NULL default '0',
-  PRIMARY KEY  (`id`),
-  UNIQUE KEY `nick` (`nick`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+CREATE TABLE nickname (
+  id              INTEGER PRIMARY KEY,
+  nick            VARCHAR(30) NOT NULL default '',
+  password        VARCHAR(34) NOT NULL default '',
+  url             VARCHAR(255) NOT NULL default '',
+  email           VARCHAR(255) NOT NULL default '',
+  last_host       VARCHAR(255) NOT NULL default '',
+  last_realname   VARCHAR(255) NOT NULL default '',
+  last_quit       VARCHAR(255) NOT NULL default '',
+  last_quit_time  INTEGER NOT NULL default '0',
+  reg_time        INTEGER NOT NULL default '0',
+  last_seen       INTEGER NOT NULL default '0',
+  last_used       INTEGER NOT NULL default '0',
+  status          INTEGER NOT NULL default '0',
+  flags           INTEGER NOT NULL default '0',
+  language        INTEGER NOT NULL default '0',
+  UNIQUE (nick)
+)ENGINE=MyISAM;
