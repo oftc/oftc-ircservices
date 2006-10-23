5f443c2641c16f476056f1cfccbe2d2fcff6abe0
Stuart Walsh <stu@oftc.net>
2006-10-23 16:04:46 +0000

    add chanserv table for pgsql (which no doubt weasel wont approve of)

---
+++ sql/chanserv-pgsql.sql
@@ -0,0 +1,7 @@
+CREATE TABLE channel(
+  id              SERIAL PRIMARY KEY,
+  channel         VARCHAR(30) NOT NULL default '',
+  founder         INTEGER NOT NULL default 0,
+  FOREIGN KEY (founder) REFERENCES nickname (id),
+  UNIQUE (channel)
+);
