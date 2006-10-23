bfdecb11a79ff78caa48ba845070bd2cc6702a9f
Rico Gloeckner <many@oftc.net>
2006-10-23 16:10:38 +0000

    i guess i forgot these definitions

--- include/chanserv.h
+++ include/chanserv.h
@@ -11,8 +11,12 @@ struct RegChannel
 };
 
 #define CS_HELP_REG_SHORT 1
-#define CS_HELP_REG_LONG 2
-#define CS_HELP_SHORT 3 
-#define CS_HELP_LONG 4
+#define CS_HELP_REG_LONG  2
+#define CS_HELP_SHORT     3 
+#define CS_HELP_LONG      4
+#define CS_REG_NS_FIRST   5
+#define CS_ALREADY_REG    6
+#define CS_REG_SUCCESS    7
+#define CS_REG_FAIL       8
 
 #endif
