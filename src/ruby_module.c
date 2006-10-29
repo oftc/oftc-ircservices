3c3b53d4eac272e20dd1ee29fbcda131fd846220
Timothy J. Fontaine <tjfontaine@oftc.net>
2006-10-29 20:05:37 +0000

    bring ruby stuff up to date, handle some signals better, namely SIGINT also handle SIGABRT to display errors when ruby exits unhappily

--- src/ruby_module.c
+++ src/ruby_module.c
@@ -1,14 +1,57 @@
 #include <ruby.h>
+#include <ctype.h>
+#include <signal.h>
 #include "stdinc.h"
 
 VALUE mOftc;
 VALUE cModule;
 
+char *strupr(char *s)
+{
+	char *c;
+	for (c=s; c && *c; c++) if (*c >= 'a' && *c <= 'z') *c -= 32;
+	return c;
+}
+
+static void
+m_generic(struct Service *service, struct Client *client,
+		    int parc, char *parv[])
+{
+	char *command = strdup(service->last_command);
+	VALUE *rbservice, *rbclient, *rbparv;
+	VALUE cServiceStruct, cClientStruct, class;
+	int i;
+
+	strupr(command);
+
+	class = rb_path2class(service->name);
+		
+	printf("Ruby Generic Handler Call: %s -> %s from %s\n", service->name, command, parv[0]);
+
+	rbparv = rb_ary_new();
+
+	for (i = 1; i <= parc; ++i)
+		rb_ary_push(rbparv, rb_str_new2(parv[i]));
+
+	cServiceStruct = rb_path2class("ServiceStruct");
+	cClientStruct = rb_path2class("ClientStruct");
+
+	rbservice = Data_Wrap_Struct(cServiceStruct, 0, free, client);
+	rbclient = Data_Wrap_Struct(cClientStruct, 0, free, client);
+	
+	printf("Passing Control to Ruby Handler\n");
+	rb_funcall2(class, rb_intern(command), 3, (VALUE *)(&rbservice, &rbclient, &rbparv));
+}
+
 VALUE Module_register(int argc, VALUE *argv, VALUE class)
 {
 	struct Service *service;
+	struct ServiceMessage *generic_msgtab;
+	VALUE aryCommands, command;
+	long i;
+	int n;
 
-	service = make_service(STR2CSTR(argv[0]));
+	service = make_service(StringValueCStr(argv[0]));
 	clear_serv_tree_parse(&service->msg_tree);
 	dlinkAdd(service, &service->node, &services_list);
 	hash_add_service(service);
@@ -16,12 +59,40 @@ VALUE Module_register(int argc, VALUE *argv, VALUE class)
 
 	/*load_language(chanserv, "chanserv.en");
 	load_language(chanserv, "chanserv.rude");
-	load_language(chanserv, "chanserv.de");
+	load_language(chanserv, "chanserv.de");*/
 	
-	mod_add_servcmd(&chanserv->msg_tree, &register_msgtab);
-	mod_add_servcmd(&chanserv->msg_tree, &help_msgtab);*/
+	Check_Type(argv[1], T_ARRAY);
+	
+	aryCommands = argv[1];
+	
+	for(i = RARRAY(aryCommands)->len-1; i >= 0; --i)
+	{
+		generic_msgtab = MyMalloc(sizeof(struct ServiceMessage));
+		command = rb_ary_shift(aryCommands);
+		generic_msgtab->cmd = StringValueCStr(command);
+		rb_ary_push(aryCommands, command);
 
+		for(n = 0; n < SERVICES_LAST_HANDLER_TYPE; n++)
+			generic_msgtab->handlers[n] = m_generic;
+		
+		printf("Adding generic ruby handler for: %s\n", generic_msgtab->cmd);
+		
+		mod_add_servcmd(&service->msg_tree, generic_msgtab);
+	}
+		
 	return Qnil;
+}
+
+VALUE Module_reply_user(int argc, VALUE *argv, VALUE class)
+{
+	struct Service *service;
+	struct Client *client;
+	
+	Data_Get_Struct(argv[0], struct Service, service);
+	Data_Get_Struct(argv[1], struct Client, client);
+	char *message = StringValueCStr(argv[2]);
+	
+	reply_user(service, client, message);
 }
 
 void Init_Module(void)
@@ -29,6 +100,7 @@ void Init_Module(void)
 	mOftc = rb_define_module("Oftc");
 	cModule = rb_define_class_under(mOftc,"ModuleServer", rb_cObject);
 	rb_define_singleton_method(cModule, "register", Module_register, -1);
+	rb_define_singleton_method(cModule, "repy_user", Module_reply_user, -1);
 }
 
 int
@@ -51,9 +123,6 @@ load_ruby_module(const char *name, const char *dir, const char *fname)
 	}
 	
 	status = ruby_exec();
-	klass = rb_const_get(rb_cObject, rb_intern(classname));
-	self = rb_class_new_instance(0, NULL, klass);
-	rb_funcall(self, rb_intern("moo"), 0, NULL);
 	
 	strncpy(classname, fname, strlen(fname)-3);
 	
@@ -72,6 +141,8 @@ load_ruby_module(const char *name, const char *dir, const char *fname)
 	return 1;
 }
 
+void sigabrt() { printf("SIGABRT\n"); rb_p(ruby_errinfo); }
+
 void
 init_ruby(void)
 {
@@ -79,4 +150,8 @@ init_ruby(void)
 	ruby_show_version();
 	ruby_init_loadpath();
 	Init_Module();
+	signal(SIGINT, SIG_DFL);
+	signal(SIGHUP, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+	signal(SIGABRT, sigabrt);
 }
