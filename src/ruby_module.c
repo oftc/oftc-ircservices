5c8bb64a0081ade04f0e8c228a00a6d9c89c0a76
Timothy J. Fontaine <tjfontaine@oftc.net>
2006-10-29 21:14:19 +0000

    remove some printfs, handle parameter passing properly, use find_service to reply

--- src/ruby_module.c
+++ src/ruby_module.c
@@ -18,44 +18,42 @@ m_generic(struct Service *service, struct Client *client,
 		    int parc, char *parv[])
 {
 	char *command = strdup(service->last_command);
-	VALUE *rbservice, *rbclient, *rbparv;
-	VALUE cServiceStruct, cClientStruct, class;
+	VALUE rbparams, rbclient, rbparv;
+	VALUE cClientStruct, class;
 	int i;
 
 	strupr(command);
 
 	class = rb_path2class(service->name);
 		
-	printf("Ruby Generic Handler Call: %s -> %s from %s\n", service->name, command, parv[0]);
-
+	rbparams = rb_ary_new();
 	rbparv = rb_ary_new();
 
 	for (i = 1; i <= parc; ++i)
 		rb_ary_push(rbparv, rb_str_new2(parv[i]));
 
-	cServiceStruct = rb_path2class("ServiceStruct");
 	cClientStruct = rb_path2class("ClientStruct");
+	rbclient = Data_Wrap_Struct(rb_cObject, 0, free, client);
 
-	rbservice = Data_Wrap_Struct(cServiceStruct, 0, free, client);
-	rbclient = Data_Wrap_Struct(cClientStruct, 0, free, client);
+	rb_ary_push(rbparams, rbclient);
+	rb_ary_push(rbparams, rbparv);
 	
-	printf("Passing Control to Ruby Handler\n");
-	rb_funcall2(class, rb_intern(command), 3, (VALUE *)(&rbservice, &rbclient, &rbparv));
+	rb_funcall2(class, rb_intern(command), RARRAY(rbparams)->len, RARRAY(rbparams)->ptr);
 }
 
 VALUE Module_register(int argc, VALUE *argv, VALUE class)
 {
-	struct Service *service;
+	struct Service *ruby_service;
 	struct ServiceMessage *generic_msgtab;
 	VALUE aryCommands, command;
 	long i;
 	int n;
 
-	service = make_service(StringValueCStr(argv[0]));
-	clear_serv_tree_parse(&service->msg_tree);
-	dlinkAdd(service, &service->node, &services_list);
-	hash_add_service(service);
-	introduce_service(service);
+	ruby_service = make_service(StringValueCStr(argv[0]));
+	clear_serv_tree_parse(&ruby_service->msg_tree);
+	dlinkAdd(ruby_service, &ruby_service->node, &services_list);
+	hash_add_service(ruby_service);
+	introduce_service(ruby_service);
 
 	/*load_language(chanserv, "chanserv.en");
 	load_language(chanserv, "chanserv.rude");
@@ -75,9 +73,7 @@ VALUE Module_register(int argc, VALUE *argv, VALUE class)
 		for(n = 0; n < SERVICES_LAST_HANDLER_TYPE; n++)
 			generic_msgtab->handlers[n] = m_generic;
 		
-		printf("Adding generic ruby handler for: %s\n", generic_msgtab->cmd);
-		
-		mod_add_servcmd(&service->msg_tree, generic_msgtab);
+		mod_add_servcmd(&ruby_service->msg_tree, generic_msgtab);
 	}
 		
 	return Qnil;
@@ -85,13 +81,14 @@ VALUE Module_register(int argc, VALUE *argv, VALUE class)
 
 VALUE Module_reply_user(int argc, VALUE *argv, VALUE class)
 {
-	struct Service *service;
 	struct Client *client;
+	struct Service *service;
 	
-	Data_Get_Struct(argv[0], struct Service, service);
+	service = find_service(StringValueCStr(argv[0]));
+	
 	Data_Get_Struct(argv[1], struct Client, client);
 	char *message = StringValueCStr(argv[2]);
-	
+
 	reply_user(service, client, message);
 }
 
@@ -100,7 +97,7 @@ void Init_Module(void)
 	mOftc = rb_define_module("Oftc");
 	cModule = rb_define_class_under(mOftc,"ModuleServer", rb_cObject);
 	rb_define_singleton_method(cModule, "register", Module_register, -1);
-	rb_define_singleton_method(cModule, "repy_user", Module_reply_user, -1);
+	rb_define_singleton_method(cModule, "reply_user", Module_reply_user, -1);
 }
 
 int
@@ -126,7 +123,6 @@ load_ruby_module(const char *name, const char *dir, const char *fname)
 	
 	strncpy(classname, fname, strlen(fname)-3);
 	
-	printf("Loading ruby class: %s\n", classname);
 	klass = rb_protect((VALUE (*)())rb_path2class, (VALUE)(classname), &status);
 	
 	if(status != 0)
@@ -134,14 +130,13 @@ load_ruby_module(const char *name, const char *dir, const char *fname)
 		rb_p(ruby_errinfo);
 		return 0;
 	}
-	printf("Ruby class loaded\n");
 	
 	self = rb_funcall2(klass, rb_intern("new"), 0, NULL);
 	
 	return 1;
 }
 
-void sigabrt() { printf("SIGABRT\n"); rb_p(ruby_errinfo); }
+void sigabrt() { printf("We've encountered a SIGABRT\n"); rb_p(ruby_errinfo); }
 
 void
 init_ruby(void)
