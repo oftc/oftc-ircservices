325dff50706af6a16432a10cedc7fb73eb67f8bf
Timothy J. Fontaine <tjfontaine@oftc.net>
2006-10-31 18:03:18 +0000

    this adds the new mc server callback hooks to ruby, and updates RubyServ with an example

--- modules/RubyServ.rb
+++ modules/RubyServ.rb
@@ -2,13 +2,26 @@ class RubyServ < ServiceModule
 	def initialize
 		service_name("RubyServ")
 		register(["HELP", "SAY"])
+		add_cmode_hook('cmode')
+		add_umode_hook('umode')
 	end
 	def HELP(client, parv = [])
 		puts "RubyServ::Help"
 	end
 	def SAY(client, parv = [])
+		parv.shift
 		message = parv.join(" ")
 		reply_user(client, client.name + " Said: "+message)
+	end
+	def umode(clientp, sourcep, params)
+		puts "Made it to UMODE"
+		puts "UMODE clientp.name: "+ clientp.name + " sourcep.name: "+ sourcep.name
+		puts params.join(" ")
+	end
+	def cmode(clientp, sourcep, channel, params)
+		puts "Made it to CMODE"
+		puts "CMODE clientp.name: "+ clientp.name + " sourcep.name: "+ sourcep.name
+		puts params.join(" ")
 	end
 end
 
--- src/ruby_module.c
+++ src/ruby_module.c
@@ -29,8 +29,16 @@
 
 #define RB_CALLBACK(x) (VALUE (*)())(x)
 
-VALUE cServiceModule;
-VALUE cClientStruct;
+VALUE cServiceModule = Qnil;
+VALUE cClientStruct = Qnil;
+
+VALUE ruby_server_hooks = Qnil;
+
+#define RB_HOOKS_CMODE 0
+#define RB_HOOKS_UMODE 1
+
+static dlink_node *ruby_cmode_hook;
+static dlink_node *ruby_umode_hook;
 
 char *strupr(char *s)
 {
@@ -71,7 +79,7 @@ ruby_handle_error(int status)
 	if(status)
 	{
 		ruby_script_error();
-		ruby_cleanup(status);
+		//ruby_cleanup(status);
 	}
 	return status;
 }
@@ -96,28 +104,12 @@ rb_singleton_call(VALUE data)
 	return rb_funcall2(args[0], args[1], args[2], args[3]);
 }
 
-static void
-m_generic(struct Service *service, struct Client *client,
-		    int parc, char *parv[])
+VALUE
+rb_cclient2rbclient(struct Client *client)
 {
-	char *command = strdup(service->last_command);
-	VALUE rbparams, rbclient, rbparv;
-	VALUE class, real_client, self;
-	VALUE fc2params[4];
-	ID class_command;
-	int i, status;
-
-	strupr(command);
-	class_command = rb_intern(command);
-
-	class = rb_path2class(service->name);
+	VALUE fc2params[4], rbclient, real_client;
+	int status;
 	
-	rbparams = rb_ary_new();
-	rbparv = rb_ary_new();
-
-	for (i = 1; i <= parc; ++i)
-		rb_ary_push(rbparv, rb_str_new2(parv[i]));
-
 	rbclient = Data_Wrap_Struct(rb_cObject, 0, free, client);
 
 	fc2params[0] = cClientStruct;
@@ -129,10 +121,54 @@ m_generic(struct Service *service, struct Client *client,
 
 	if(ruby_handle_error(status))
 	{
-		reply_user(service, client, "An error has occurred, please be patient and report this bug");
-		global_notice(service, "Ruby Failed To Create ClientStruct");
 		printf("RUBY ERROR: Ruby Failed To Create ClientStruct\n");
+		return Qnil;
 	}
+
+	return real_client;
+}
+
+VALUE
+rb_carray2rbarray(int parc, char **parv)
+{
+	if(parv)
+	{
+		VALUE rbarray = rb_ary_new2(parc);
+		int i;
+		char *cur;
+		
+		for (i = 0; i <= parc; ++i)
+		{
+			cur = parv[i];
+			if(cur)
+				rb_ary_push(rbarray, rb_str_new2(cur));
+		}
+		
+		return rbarray;
+	}
+	else
+		return rb_ary_new();
+}
+
+static void
+m_generic(struct Service *service, struct Client *client,
+		    int parc, char *parv[])
+{
+	char *command = strdup(service->last_command);
+	VALUE rbparams, rbparv;
+	VALUE class, real_client, self;
+	VALUE fc2params[4];
+	ID class_command;
+
+	strupr(command);
+	class_command = rb_intern(command);
+
+	class = rb_path2class(service->name);
+	
+	rbparams = rb_ary_new();
+
+	real_client = rb_cclient2rbclient(client);
+	rbparv = rb_carray2rbarray(parc, parv);
 
 	rb_ary_push(rbparams, real_client);
 	rb_ary_push(rbparams, rbparv);
@@ -215,6 +251,30 @@ ServiceModule_service_name(VALUE self, VALUE name)
 	return rb_iv_set(self, "@ServiceName", name);
 }
 
+VALUE
+ServiceModule_cmode_hook(VALUE self, VALUE hook)
+{
+	VALUE hooks = rb_ary_entry(ruby_server_hooks, RB_HOOKS_CMODE);
+	VALUE newhook = rb_ary_new();
+	rb_ary_push(newhook, rb_iv_get(self, "@ServiceName"));
+	rb_ary_push(newhook, self);
+	rb_ary_push(newhook, hook);
+	rb_ary_push(hooks, newhook);
+	return Qnil;
+}
+
+VALUE
+ServiceModule_umode_hook(VALUE self, VALUE hook)
+{
+	VALUE hooks = rb_ary_entry(ruby_server_hooks, RB_HOOKS_UMODE);
+	VALUE newhook = rb_ary_new();
+	rb_ary_push(newhook, rb_iv_get(self, "@ServiceName"));
+	rb_ary_push(newhook, self);
+	rb_ary_push(newhook, hook);
+	rb_ary_push(hooks, newhook);
+	return Qnil;
+}
+
 void
 Init_Module(void)
 {
@@ -223,6 +283,8 @@ Init_Module(void)
 	rb_define_method(cServiceModule, "register", ServiceModule_register, 1);
 	rb_define_method(cServiceModule, "reply_user", ServiceModule_reply_user, 2);
 	rb_define_method(cServiceModule, "service_name", ServiceModule_service_name, 1);
+	rb_define_method(cServiceModule, "add_cmode_hook", ServiceModule_cmode_hook, 1);
+	rb_define_method(cServiceModule, "add_umode_hook", ServiceModule_umode_hook, 1);
 }
 
 VALUE
@@ -290,6 +352,101 @@ Init_ClientStruct(void)
 	rb_define_method(cClientStruct, "umodes", ClientStruct_Umodes, 0);
 }
 
+static void *
+rb_cmode_hdlr(va_list args)
+{
+	struct Client  *client_p = va_arg(args, struct Client*);
+	struct Client  *source_p = va_arg(args, struct Client*);
+	struct Channel *chptr    = va_arg(args, struct Channel*);
+	int             parc     = va_arg(args, int);
+	char           **parv    = va_arg(args, char **);
+
+	VALUE hooks = rb_ary_entry(ruby_server_hooks, RB_HOOKS_CMODE);
+
+	if(RARRAY(hooks)->len)
+	{
+		VALUE params, current, service_name, self, command;
+		VALUE fc2params[4];
+		
+		ID command_id;
+		int i;
+
+		params = rb_ary_new();
+
+		rb_ary_push(params, rb_cclient2rbclient(client_p));
+		rb_ary_push(params, rb_cclient2rbclient(source_p));
+		//printf("RUBY INFO: Get channel\n");
+		//rb_ary_push(params,rb_cchannel2rbchannel(chptr));
+		rb_ary_push(params, Qnil);
+		rb_ary_push(params, rb_carray2rbarray(parc, parv));
+
+		for(i = 0; i < RARRAY(hooks)->len; ++i)
+		{
+			current = rb_ary_entry(hooks, i);
+			service_name = rb_ary_entry(current, 0);
+			self = rb_ary_entry(current, 1);
+			command = rb_ary_entry(current, 2);
+			command_id = rb_intern(StringValueCStr(command));
+			
+			fc2params[0] = self;
+			fc2params[1] = command_id;
+			fc2params[2] = RARRAY(params)->len;
+			fc2params[3] = RARRAY(params)->ptr;
+
+			if(!do_ruby(RB_CALLBACK(rb_singleton_call), (VALUE)fc2params))
+			{
+				printf("RUBY ERROR: Failed to call: %s\n", StringValueCStr(command));
+			}
+		}
+	}
+	
+	pass_callback(ruby_cmode_hook);
+}
+
+static void*
+rb_umode_hdlr(va_list args)
+{
+	struct Client *client_p = va_arg(args, struct Client*);
+	struct Client *source_p = va_arg(args, struct Client*);
+	int            parc     = va_arg(args, int);
+	char         **parv     = va_arg(args, char**);
+
+	VALUE hooks = rb_ary_entry(ruby_server_hooks, RB_HOOKS_UMODE);
+
+	if(RARRAY(hooks)->len)
+	{
+		VALUE params, current, service_name, self, command;
+		VALUE fc2params[4];
+		ID command_id;
+		int i;
+
+		rb_ary_push(params, rb_cclient2rbclient(client_p));
+		rb_ary_push(params, rb_cclient2rbclient(source_p));
+		rb_ary_push(params, rb_carray2rbarray(parc, parv));
+		
+		for(i = 0; i < RARRAY(hooks)->len; ++i)
+		{
+			current = rb_ary_entry(hooks, i);
+			service_name = rb_ary_entry(current, 0);
+			self = rb_ary_entry(current, 1);
+			command = rb_ary_entry(current, 2);
+			command_id = rb_intern(StringValueCStr(command));
+
+			fc2params[0] = self;
+			fc2params[1] = command_id;
+			fc2params[2] = RARRAY(params)->len;
+			fc2params[3] = RARRAY(params)->ptr;
+
+			if(!do_ruby(RB_CALLBACK(rb_singleton_call), (VALUE)fc2params))
+			{
+				printf("RUBY ERROR: Failed to call: %s\n", StringValueCStr(command));
+			}
+		}
+	}
+	
+	pass_callback(ruby_umode_hook);
+}
+
 int
 load_ruby_module(const char *name, const char *dir, const char *fname)
 {
@@ -331,6 +488,9 @@ load_ruby_module(const char *name, const char *dir, const char *fname)
 
 	service = find_service(classname);
 	service->data = (void *)self;
+
+	ruby_cmode_hook = install_hook(cmode_hook, rb_cmode_hdlr);
+	ruby_umode_hook = install_hook(umode_hook, rb_umode_hdlr);
 	
 	return 1;
 }
@@ -355,4 +515,8 @@ init_ruby(void)
 	signal(SIGHUP, SIG_DFL);
 	signal(SIGQUIT, SIG_DFL);
 	signal(SIGABRT, sigabrt);
+
+	ruby_server_hooks = rb_ary_new();
+	rb_ary_push(ruby_server_hooks, rb_ary_new());
+	rb_ary_push(ruby_server_hooks, rb_ary_new());
 }
