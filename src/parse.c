464a28b5af8c13b2f98da2995756eadd303684b0
Stuart Walsh <stu@oftc.net>
2006-11-01 19:06:15 +0000

    r196@helium:  stu | 2006-11-01 19:06:07 +0000  oops

--- src/parse.c
+++ src/parse.c
@@ -304,7 +304,7 @@ handle_command(struct Message *mptr, struct Client *client,
     exit_client(client, client, "Not enough arguments to server command.");
   }
   else
-    (*handler)(client, from, i, hpara);
+    (*handler)(from, client, i, hpara);
 }
 
 static void
