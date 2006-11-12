5c77a132837421d97162730de86be48b83fcc44a
Rico Gloeckner <many@oftc.net>
2006-11-12 10:35:37 +0000

    sync postgres/chanserv schema

--- sql/chanserv-pgsql.sql
+++ sql/chanserv-pgsql.sql
@@ -3,6 +3,9 @@ CREATE TABLE channel(
   id              SERIAL PRIMARY KEY,
   channel         VARCHAR(30) NOT NULL default '',
   founder         INTEGER NOT NULL default 0,
+  successor       INTEGER NOT NULL default 0,
+  description     VARCHAR(255) NOT NULL default '',
+  url             VARCHAR(255) NOT NULL default '',
   FOREIGN KEY (founder) REFERENCES nickname (id),
   UNIQUE (channel)
 );
