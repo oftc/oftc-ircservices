8b43ac30e470e530aab21379d5db48dd0863646e
Stuart Walsh <stu@oftc.net>
2006-11-20 21:23:02 +0000

    r1250@helium:  stu | 2006-11-20 21:23:01 +0000  it's always a good idea to test things before you commit them

--- sql/operserv-pgsql.sql
+++ sql/operserv-pgsql.sql
@@ -3,9 +3,8 @@ CREATE TABLE akill (
   id              SERIAL PRIMARY KEY,
   mask            VARCHAR(255) NOT NULL DEFAULT '',
   reason          VARCHAR(255) NOT NULL DEFAULT '',
-  setter          INTEGER NOT NULL DEFAULT 0,
+  setter          INTEGER REFERENCES nickname(id),
   time            INTEGER NOT NULL DEFAULT 0,
   duration        INTEGER NOT NULL DEFAULT 0,
-  PRIMARY KEY  (id),
   UNIQUE (mask)
 );
