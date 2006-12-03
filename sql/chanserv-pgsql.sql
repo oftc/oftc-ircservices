e0d523c7f9e1489ebb4456c02cf6520d26f19941
Peter Palfrader <weasel@oftc.net>
2006-12-03 22:14:03 +0000

    channel flags

--- sql/chanserv-pgsql.sql
+++ sql/chanserv-pgsql.sql
@@ -1,29 +1,29 @@
 DROP TABLE channel;
 CREATE TABLE channel(
-  id              SERIAL PRIMARY KEY,
-  channel         VARCHAR(30) NOT NULL default '',
-  founder         INTEGER NOT NULL default 0,
-  successor       INTEGER NOT NULL default 0,
-  flags           INTEGER NOT NULL default 0,
-  description     VARCHAR(255) NOT NULL default '',
-  url             VARCHAR(255) NOT NULL default '',
-  email           VARCHAR(255) NOT NULL default '',
-  entrymsg        VARCHAR(255) NOT NULL default '',
-  topic           VARCHAR(255) NOT NULL default '',
-
-  FOREIGN KEY (founder) REFERENCES nickname (id),
+  id                    SERIAL PRIMARY KEY,
+  channel               VARCHAR(30) NOT NULL,
+  founder               INTEGER NOT NULL REFERENCES account(id),
+  successor             INTEGER REFERENCES account(id),
+  flag_forbidden        BOOLEAN NOT NULL DEFAULT 'False', -- channel is forbidden.  it may not be used
+  flag_private          BOOLEAN NOT NULL DEFAULT 'False', -- do not show up in list of channels
+  flag_restricted_ops   BOOLEAN NOT NULL DEFAULT 'False', -- only people on the access list can hold channel operator status
+  flag_topic_lock       BOOLEAN NOT NULL DEFAULT 'False', -- topics can only be changed via chanserv
+  flag_secure           BOOLEAN NOT NULL DEFAULT 'False', -- only people who have identified with their password (not by access list) can use their privileged channel access
+  flag_verbose          BOOLEAN NOT NULL DEFAULT 'False', -- notice all chanserv actions to the channel
+  description           VARCHAR(255),
+  url                   VARCHAR(255),
+  email                 VARCHAR(255),
+  entrymsg              VARCHAR(255),
+  topic                 VARCHAR(512),
   UNIQUE (channel)
 );
 
 DROP TABLE channel_access;
 CREATE TABLE channel_access(
-  id             SERIAL PRIMARY KEY,
-  channel_id     INTEGER NOT NULL default '0',
-  nick_id        INTEGER NOT NULL default '0',
-  level          INTEGER NOT NULL default '0',
-
-  FOREIGN KEY (channel_id) REFERENCES channel (id),
-  FOREIGN KEY (nick_id) REFERENCES nickname (id),
+  id                   SERIAL PRIMARY KEY,
+  channel_id           INTEGER NOT NULL REFERENCES channel (id),
+  nick_id              INTEGER NOT NULL REFERENCES account(id),
+  level                INTEGER NOT NULL,
 
   UNIQUE (channel_id, nick_id)
-};
+);
