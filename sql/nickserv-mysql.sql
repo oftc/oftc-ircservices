b0b168de9a838130b06b8049f7a1e5eda3754c3d
Stuart Walsh <stu@oftc.net>
2006-12-27 22:36:58 +0000

    r394@deepfreeze:  stu | 2006-12-14 19:33:52 +0000  Fix mysql schema for nicks and add the new ?B type thingies for yada and other misc fixes

--- sql/nickserv-mysql.sql
+++ sql/nickserv-mysql.sql
@@ -1,11 +1,8 @@
 DROP TABLE IF EXISTS nickname CASCADE;
 DROP TABLE IF EXISTS account_access CASCADE;
 DROP TABLE IF EXISTS account CASCADE;
-DROP TABLE IF EXISTS forbidden_nickname;
-
 CREATE TABLE account (
-  id                  INTEGER PRIMARY KEY auto_increment,
-  primary_nick        INTEGER
+  id              INTEGER PRIMARY KEY auto_increment,
   password            CHAR(40),      -- base16 encoded sha1(salt+<userpassword>).  lower case
   salt                CHAR(16),
   url                 VARCHAR(255),
@@ -14,6 +11,7 @@ CREATE TABLE account (
   flag_enforce        BOOLEAN NOT NULL DEFAULT FALSE,
   flag_secure         BOOLEAN NOT NULL DEFAULT FALSE,
   flag_verified       BOOLEAN NOT NULL DEFAULT FALSE,
+  flag_forbidden      BOOLEAN NOT NULL DEFAULT FALSE,
   flag_cloak_enabled  BOOLEAN NOT NULL DEFAULT FALSE,
   flag_admin          BOOLEAN NOT NULL DEFAULT FALSE,
   flag_email_verified BOOLEAN NOT NULL DEFAULT FALSE,
@@ -22,12 +20,10 @@ CREATE TABLE account (
   last_realname       VARCHAR(255),
   last_quit_msg       VARCHAR(512),
   last_quit_time      INTEGER,
-  reg_time            INTEGER NOT NULL, -- The account itself
-  FOREIGN KEY (primary_nick) REFERENCES nickname(id)
+  reg_time            INTEGER NOT NULL -- The account itself
 ) ENGINE=InnoDB;
 
 CREATE TABLE nickname (
-  id                  INTEGER PRIMARY KEY auto_increment,
   nick                VARCHAR(255) NOT NULL,
   user_id             INTEGER NOT NULL,
   reg_time            INTEGER NOT NULL, -- This nickname
@@ -36,13 +32,9 @@ CREATE TABLE nickname (
   FOREIGN KEY (user_id) REFERENCES account(id)
 ) ENGINE=InnodB;
 
-CREATE TABLE forbidden_nickname (
-  nick                VARCHAR(255) NOT NULL PRIMARY KEY
-) ENGINE=InnoDB;
-
 CREATE TABLE account_access (
   id              INTEGER PRIMARY KEY auto_increment,
   parent_id       INTEGER NOT NULL,
   entry           VARCHAR(255) NOT NULL,
-  FOREIGN KEY (parent_id) REFERENCES account(id) ON DELETE CASCADE
+  FOREIGN KEY (parent_id) REFERENCES account(id)
 ) ENGINE=InnoDB;
