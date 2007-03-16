40a3290a308e5246c77814dac4a8591420788579
Timothy J. Fontaine <tjfontaine@oftc.net>
2007-03-16 00:52:24 +0000

    let us make the trunk compile  * Makefile.am put languages before modules so modules can include their languages  * include/services.h remove language definitions and include services-lang.h

--- include/services.h
+++ include/services.h
@@ -5,17 +5,7 @@
 
 void services_die(const char *, int);
 
-#define SERV_HELP_NOT_AVAIL 1
-#define SERV_SUB_HELP_NOT_AVIL 2
-#define SERV_HELP_SHORT 3
-#define SERV_UNKNOWN_CMD 4
-#define SERV_INSUFF_PARAM 5
-#define SERV_NOT_IDENTIFIED 6
-#define SERV_ACCESS_DENIED 7
-#define SERV_UNREG_CHAN 8
-#define SERV_NO_ACCESS_CHAN 9
-#define SERV_NO_ACCESS 10
-#define SERV_UNKNOWN_OPTION 11
+#include "services-lang.h"
 
 struct ServicesState_t
 {
