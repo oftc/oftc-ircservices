ec2cbee148ebb330abebf219d1e4be736f006a25
Timothy J. Fontaine <tjfontaine@oftc.net>
2007-03-21 00:34:42 +0000

    almost verbatim copy of stu's dconvertdb.d, hasn't been tested, surely will have typos, requires libdbd-mysql-ruby and libdbd-pg-ruby

---
+++ migrate-db.rb
@@ -0,0 +1,254 @@
+#!/usr/bin/ruby
+#old services migration script, requires libdbd-mysql-ruby libdbd-pg-ruby
+#specify a yaml file on the cli that you use as a config, see migrate-db.yaml
+
+require 'dbi'
+require 'yaml'
+
+NI_ENFORCE      = 0x1
+NI_SECURE       = 0x2
+NI_PRIVATE      = 0x40
+NI_CLOAK        = 0x100000
+
+CI_PRIVATE      = 0x4
+CI_TOPICLOCK    = 0x8
+CI_RESTRICTED   = 0x10
+CI_VERBOTEN     = 0x80
+CI_VERBOSE      = 0x800
+
+$oftcid = -1
+
+$nicks = []
+$channels = []
+
+def bit_check(flags, level)
+  if (flags & level > 0) then true else false end
+end
+
+def process_nicks()
+  source_handle = $source.prepare("SELECT pass, salt, url, email, 
+    cloak_string, last_usermask, last_realname, last_quit, last_quit_time, 
+    time_registered, nick, last_seen, flags, nick_id, link_id FROM nick 
+    ORDER BY link_id")
+  source_handle.execute
+
+  while row = source_handle.fetch do
+    if row["link_id"] != 0
+      linkid = row["link_id"].to_i
+      if $nicks.include?(linkid)
+        insert_handle = $dest.prepare("INSERT INTO nickname(nick, user_id,
+          reg_time, last_seen) VALUES(?, ?, ?, ?)")
+
+        insert_handle.execute(row["nick"],
+                              $nicks[linkid],
+                              row["time_registered"],
+                              row["last_seen"])
+
+        $nicks[row["nick_id"].to_i] = $nicks[linkid]
+
+        insert_handle.finish
+      else
+        puts "link id not in yet retrieved"
+        exit(-1)
+      end
+    else
+      insert_handle = $dest.prepare("INSERT INTO account(password, salt, url, 
+        email, cloak, last_host, last_realname, last_quit_msg, 
+        last_quit_time, reg_time, flag_cloak_enabled, flag_secure,
+        flag_enforce, flag_private) 
+        VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+
+      flags = row["flags"].to_i
+      flag_enforce = bit_check(flags, NI_ENFORCE)
+      flag_secure  = bit_check(flags, NI_SECURE)
+      flag_cloak   = bit_check(flags, NI_CLOAK)
+      flag_private = bit_check(flags, NI_PRIVATE)
+
+      insert_handle.execute(row["pass"], row["salt"], row["url"], row["email"],
+        row["cloak_string"], row["last_usermask"], row["last_realname"],
+        row["last_quit"], row["last_quit_time"], row["time_registered"],
+        row["nick"], row["last_seen"], flag_enforce, flag_secure, flag_cloak,
+        flag_private)
+
+      insert_handle.finish
+
+      insert_handle = $dest.prepare("INSERT INTO nickname(nick, user_id,
+        reg_time, last_seen) VALUES(?, ?, ?, ?)")
+
+      accid = $dest.select_one("SELECT currval('account_id_seq')")
+
+      $oftcid = row["nick_id"] if row["nick"] == "OFTC"
+
+      insert_handle.execute(row["nick"], accid[0], row["time_registered"],
+        row["last_seen"])
+
+      insert_handle.finish
+
+      $nicks[row["nick_id"].to_i] = accid[0].to_i
+
+      nickid = $dest.select_one("SELECT currval('nickname_id_seq')")
+      $dest.execute("UDPATE account SET primary_nick = ? WHERE id = ?",
+        nickid[0], accid[0])
+    end
+  end
+end
+
+def process_admins
+  handle = $source.prepare("SELECT admin_id, nick_id FROM admin")
+  handle.execute
+
+  while row = handle.fetch do
+    $dest.execute("UPDATE account SET flag_admin=true WHERE id = ?", row["nick_id"])
+  end
+
+  handle.finish
+end
+
+def process_nickaccess()
+  handle = $source.prepare("SELECT nick_id, userhost FROM nickaccess")
+  handle.execute
+
+  while row = handle.fetch do
+    $dest.execute("INSERT INTO account_access (parent_id, entry) VALUES(?, ?)",
+      row["nick_id"], row["userhost"])
+  end
+
+  handle.finish
+end
+
+def process_channels()
+  handle = $source.prepare("SELECT name, description, url, email, last_topic,
+    entry_message, time_registered, last_used, flags, channel_id, mlock_on,
+    mlock_off, mlock_limit, mlock_key, limit_offset, bantime, founder,
+    successor FROM channel")
+  handle.execute
+
+  while row = handle.fetch do
+    insert_handle = $dest.prepare("INSERT INTO channel(channel, description,
+      url, email, topic, entrymsg, reg_time, last_used, flag_private,
+      flag_restricted, flag_topic_lock, flag_verbose, flag_autolimit,
+      flag_expirebans, flag_forbidden)
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+
+    flags = row["flags"].to_i
+    flag_private = bit_check(flags, CI_PRIVATE)
+    flag_restricted = bit_check(flags, CI_RESTRICTED)
+    flag_topiclock = bit_check(flags, CI_TOPICLOCK)
+    flag_verbose = bit_check(flags, CI_VERBOSE)
+    flag_autolimit = if row["limit_offset"].to_i != 0 then true else false end
+    flag_expirebans = if row["bantime"].to_i != 0 then true else false end
+    flag_forbidden = bit_check(flags, CI_VERBOTEN)
+
+    insert_handle.execute(row["name"], row["description"], row["url"], row["email"],
+      row["last_topic"], row["entry_message"], row["time_registered"], row["last_used"],
+      flag_private, flag_restricted, flag_topiclock, flag_verbose, flag_autolimit,
+      flag_expirebans, flag_forbidden)
+
+    insert_handle.finish
+
+    cid = $dest.select_ond("SELECT currval('channel_id_seq')")
+
+    chanid = cid[0].to_i
+    oldchanid = row["channel_id"].to_i
+    founder = row["founder"].to_i
+    successor = row["successor"].to_i
+
+    $channels[oldchanid] = chanid
+
+    founder = $oftcid if founder == 0 or ! $nicks.include?(founder)
+
+    $dest.execute("INSERT INTO channel_access(channel_id, account_id, level)
+      VALUES (?, ?, ?)", chanid, $nicks[founder], 4)
+
+    if successor > 0 && founder != successor && $nicks.include?(successor) &&
+        $nicks[successor] != founder && $nicks[founder] != successor &&
+        $nicks[successor] != $nicks[founder]
+    then
+      $dest.execute("INSERT INTO channel_access(channel_id, account_id, level)
+        VALUES (?, ?, ?)", chanid, $nicks[successor], 4)
+    end
+  end
+
+  handle.finish
+end
+
+def process_channaccess()
+  handle = $source.prepare("SELECT channel.channel_id, level, nick_id, 
+    founder, successor FROM chanaccess, channel
+    WHERE chanaccess.channel_id = channel.channel_id ORDER BY channel_id")
+  handle.execute
+
+  lastid = -1
+  done = []
+
+  while row = handle.fetch do
+    chanid = row["channel_id"].to_i
+    accid = row["nick_id"].to_i
+    founder = row["founder"].to_i
+    successor = row["successor"].to_i
+    level = row["level"].to_i
+
+    done.each { |key| done.remove(key) } if lastid != chanid
+
+    next if accid == founder or 
+      accid == successor or
+      done.include?($nicks[accid]) or
+      $nicks[accid] == $nicks[founder] or
+      ($nicks.include?(successor) and $nicks[successor] == $nicks[accid])
+
+    insert_handle = $dest.prepare("INSERT INTO channel_access (channel_id,
+      account_id, level) VALUES (?, ?, ?)");
+
+      nlevel = 3
+      nlevel = 2 if level < 5
+
+    insert_handle.execute($channels[chanid], $nicks[accid], nlevel)
+
+    insert_handle.finish
+
+    done[$nicks[accid]] = true
+  end
+
+  handle.finish
+end
+
+if ARGV.length != 1
+  puts "please specify config file"
+  exit(-1)
+end
+
+conffile = ARGV.shift
+conf = YAML::load(File.open(conffile))
+
+$source = DBI.connect(conf["source_str"], conf["source_user"], conf["source_pass"])
+$dest   = DBI.connect(conf["dest_str"], conf["dest_user"], conf["dest_pass"])
+
+$dest.begin
+
+start = Time.now
+puts "process_nicks #{Time.now}"
+process_nicks()
+
+total = Time.now - start
+puts "process_admins #{Time.now} (#{total})"
+process_admins()
+
+total = Time.now - start
+puts "process_nickaccess #{Time.now} (#{total})"
+process_nickaccess()
+
+total = Time.now - start
+puts "process_channels #{Time.now} (#{total})"
+process_channels()
+
+total = Time.now - start
+puts "process_chanaccess #{Time.now} (#{total})"
+process_chanaccess()
+
+total = Time.now - start
+puts "done #{time.now} total #{total}"
+
+$dest.commit
+
+$source.disconnect
+$dest.disconnect
