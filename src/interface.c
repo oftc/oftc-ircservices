58162d68c29bef25a4169a1a232f5e9300f241af
Stuart Walsh <stu@oftc.net>
2007-03-27 19:55:58 +0000

    base16 encode returns lower case

--- src/interface.c
+++ src/interface.c
@@ -979,7 +979,7 @@ check_nick_pass(struct Nick *nick, const char *password)
   snprintf(fullpass, PASSLEN*2, "%s%s", password, nick->salt);
   
   pass = crypt_pass(fullpass, 1);
-  if(strncmp(nick->pass, pass, sizeof(nick->pass)) == 0)
+  if(strncasecmp(nick->pass, pass, sizeof(nick->pass)) == 0)
     ret = 1;
   else 
     ret = 0;
