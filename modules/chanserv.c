21b9b136aa99a64c60723cc5c7c230f5b2076389
Timothy J. Fontaine <tjfontaine@oftc.net>
2007-06-02 16:39:22 +0000

    SET #channel FLOODSERV should show on/off properly. re #29

--- modules/chanserv.c
+++ modules/chanserv.c
@@ -1704,6 +1704,7 @@ m_set_flag(struct Service *service, struct Client *client,
         break;
       case SET_CHAN_FLOODSERV:
         on = regchptr->floodserv;
+        break;
       default:
         on = FALSE;
         break;
