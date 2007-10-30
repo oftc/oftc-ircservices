00e453ab5c3cf9432d86ab9eef78c654d7cf9ce6
Stuart Walsh <stu@oftc.net>
2007-10-30 23:06:31 +0000

    commit dodgy crack-inspiried-by-weasel fix to autogen to fix libltdl problems with make dist

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,8 @@ set -e
 aclocal-1.9
 autoheader
 libtoolize --automake --ltdl
+# brokkkkkkkkkkkkkkkkkken libltdl in debian unstable
+sed -i -e 's/install-sh ltmain.sh missing mkinstalldirs/install-sh ltmain.sh missing/' libltdl/Makefile.in
 automake-1.9 --add-missing
 autoconf
 ./configure $@
