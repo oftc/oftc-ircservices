fc89d8205eb754eb75a28fe263e1fea670f82549
Stuart Walsh <stu@oftc.net>
2007-12-05 21:41:50 +0000

    [MERGE] (r1191) adding akicks got broken slightly. re #80

--- src/dbm.c
+++ src/dbm.c
@@ -913,7 +913,6 @@ db_list_add(unsigned int type, const void *value)
   struct ChanAccess *caval  = (struct ChanAccess *)value;
   struct ServiceBan *banval = (struct ServiceBan *)value;
   struct JupeEntry *jval    = (struct JupeEntry *)value;
-  unsigned int id;
   int ret = 0;
 
   switch(type)
@@ -941,7 +940,7 @@ db_list_add(unsigned int type, const void *value)
       }
       else if(banval->mask != NULL)
       {
-        db_exec(ret, INSERT_AKICK_MASK, id, banval->setter, 
+        db_exec(ret, INSERT_AKICK_MASK, banval->channel, banval->setter, 
             banval->reason, banval->mask, banval->time_set, 
             banval->duration);
       }
