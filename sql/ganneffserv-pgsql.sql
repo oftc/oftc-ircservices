13fec85e23ae951475d785c767ff41c9b301b859
Timothy J. Fontaine <tjfontaine@oftc.net>
2008-06-04 00:19:22 +0000

    add missing ganneffserv sql file

---
+++ sql/ganneffserv-pgsql.sql
@@ -0,0 +1,11 @@
+DROP TABLE ganneffserv CASCADE;
+CREATE TABLE ganneffserv (
+  id      SERIAL PRIMARY KEY,
+  setter  INTEGER REFERENCES account(id) ON DELETE SET NULL,
+  time    INTEGER NOT NULL,
+  channel VARCHAR(255) NOT NULL,
+  reason  VARCHAR(255) NOT NULL,
+  kills   INTEGER NOT NULL DEFAULT 0,
+  monitor_only BOOLEAN NOT NULL DEFAULT 'False'
+);
+CREATE UNIQUE INDEX ganneffserv_channel_idx ON channel ((lower(channel)));
